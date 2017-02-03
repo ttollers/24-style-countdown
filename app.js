c16820122c76a343c08efdaa765a7ed31b4bcc8c
ttollers <tom.tollinton@trinitymirror.com>
2017-02-03 11:13:14 +0000

    First working commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*global document */
+
+import React from 'react'
+import { render } from 'react-dom'
+import Root from "./component/index"
+
+render(
+  <Root />,
+  document.getElementById('app')
+);
---
+++ component/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Home extends React.Component {
+
+  render() {
+    return (<p>hello world</p>)
+  }
+}
+
+export default Home
