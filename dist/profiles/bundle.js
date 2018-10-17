07feea63e456107c78885aa488a7456974969004
Tom <tom@paybase.io>
2018-10-17 16:06:21 +0100

    updates

--- dist/profiles/bundle.js
+++ dist/profiles/bundle.js
@@ -155,7 +155,7 @@ const bis = {
   seed: '8',
   bio: [
     'Bis is one of the hardest people to write a bio for. Not for any emotional reasons... Simply because there isn\'t much to say.',
-    'Ever-present and enthusiastic, Bis has had many updates over the years and debates still rage on whether he his current version (2.0) has . Nevertheless, Bis has established himself as an important member of the who deserves, if not respect, at least some acknowledgement.'
+    'Ever-present and enthusiastic, Bis has had many updates over the years and debates still rage on whether his current version (2.0) is an improvement. Nevertheless, Bis has established himself as an important team member who deserves, if not respect, at least some acknowledgement.'
   ],
   strengths: [
     'Tries really hard',
@@ -169,7 +169,7 @@ const bis = {
   ],
   fifaInspiration: {
     player: 'Gutierez',
-    info: 'Never had the ability, and blighted by tragedy, Bis does the best with what he has. No one can ever understand what it\'s like for him. While we may not respect him, he earns our pity'
+    info: 'Never had the ability, and blighted by tragedy, Bis does the best with what he has. No one can ever understand what it\'s like for him. H has truly earned our pity'
   }
 };
 
